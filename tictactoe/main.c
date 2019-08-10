1707fb2353e765b2d79063819ae8f20d25e8e289
Jeremy Rand <jeremy@rand-family.com>
2019-08-09 23:33:14 -0400

    Initial Commit

---
+++ tictactoe/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * tictactoe
+ *
+ * Created by Jeremy Rand on 2019-08-09.
+ * Copyright (c) 2019 Jeremy Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
